c0af1ab8462dc78332ba560d31e87844c76f2afa
Jesus Mendoza <jesuszmendoza14@gmail.com>
2022-01-04 19:43:41 -0600

    add assets

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>This is the Home screen</Text>
+    <View style={styles.view}>
+      <View style={styles.spaceBetweenRow}>
+        <Text>Members</Text>
+        <View>
+          <Text>A</Text>
+          <Text>B</Text>
+          <Text>C</Text>
+          <TouchableOpacity style={styles.scanButton}>
+            <Text>Check In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.search}>
+        <Text>Search Members</Text>
+      </View>
+      <View style={styles.spaceBetweenRow}> 
+        <Text>348 Members</Text>
+        <View>
+          <Text>A</Text>
+          <Text>B</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  view: {
+    flex: 1,
+    backgroundColor: '#121114',
+  },
+  spaceBetweenRow: {
+
+  },
+  scanButton: {
+
+  }
 });
 
 export default Home;
